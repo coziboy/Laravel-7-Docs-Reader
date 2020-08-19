c276805f83863aad6f2e388b7fbf71f24cb1a0e1
Andreas Asatera <ryusuke005@gmail.com>
2020-08-19 09:47:21 +0700

    syntax highlight

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,7 @@
 <script src="./assets/js/jquery-3.5.1.slim.min.js" charset="utf-8"></script>
 <script src="./assets/js/popper.min.js" charset="utf-8"></script>
 <script src="./assets/js/bootstrap.min.js" charset="utf-8"></script>
+<script src="./assets/js/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 </body>
 </html>
--- components/header.php
+++ components/header.php
@@ -4,5 +4,6 @@
     <meta charset="utf-8">
     <title>Laravel Docs</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/default.min.css">
   </head>
   <body>
